d636844f9fe21fca27f6ea7a2c15feeef45a276c
thatanjan <anjan.cules@gmail.com>
2022-01-08 09:17:18 +0600

    SpaceX landing page clone project has been added.

--- src/data/projects/allProject.ts
+++ src/data/projects/allProject.ts
@@ -1,6 +1,7 @@
 import confession from 'data/projects/confession'
 import culesCoding from 'data/projects/culesCoding'
 import culesShop from 'data/projects/culesShop'
+import spacexClone from 'data/projects/spacexLandingPageClone'
 import Project from 'classes/Project/Project'
 
 export class ProjectPath {
@@ -13,7 +14,7 @@ export class ProjectPath {
 	}
 }
 
-const allProject: Project[] = [confession, culesCoding, culesShop]
+const allProject: Project[] = [confession, culesCoding, culesShop, spacexClone]
 
 export const allProjectPaths = allProject.map(
 	({ title }: Project) => new ProjectPath(title)
